cbd4caf25135cc7ff1151a8f67a401e0ff106221
analizajb <96861913+analizajb@users.noreply.github.com>
2022-07-31 20:46:46 -0700

    adding cache resources to serv-workr.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,28 @@
-const APP_PREFIX = 'FoodFest-';     
-const VERSION = 'version_01';
+const APP_PREFIX = "FoodFest-";
+const VERSION = "version_01";
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-    "\index.html",
-    "\js\idb.js",
-    "\js\index.js",
-    "\css\styles.css",
-    "\icons\icon-72x72.png",
-    "\icons\icon-96x96.png",
-    "\icons\icon-128x128.png",
-    "\icons\icon-144x144.png",
-    "\icons\icon-152x152.png",
-    "\icons\icon-192x192.png",
-    "\icons\icon-384x384.png",
-    "\icons\icon-512x512.png",
+  "index.html",
+  "jsidb.js",
+  "jsindex.js",
+  "cssstyles.css",
+  "iconsicon-72x72.png",
+  "iconsicon-96x96.png",
+  "iconsicon-128x128.png",
+  "iconsicon-144x144.png",
+  "iconsicon-152x152.png",
+  "iconsicon-192x192.png",
+  "iconsicon-384x384.png",
+  "iconsicon-512x512.png",
 ];
 
-self.addEventListener('install', function (e) {
-
-})
+// Cache resources
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
